1d98c96e7c35ddabb6d53840ba278c611fd2f2cd
Sergio Pena <isergiopena@gmail.com>
2019-05-23 17:51:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+## AWSCON
+Wrapper that displays all available EC2 instances and launchs an SSM console session for the selected one
+
+## Motivation
+I have been switching around EC2 console and AWS SSM session copy-pasting instance ids so I needed a wrapper that ease connection to EC2 instances
+
+## Build status
+Build status of continus integration i.e. travis, appveyor etc. Ex. - 
+
+[![Build Status](https://travis-ci.org/akashnimare/foco.svg?branch=master)](https://travis-ci.org/akashnimare/foco)
+[![Windows Build Status](https://ci.appveyor.com/api/projects/status/github/akashnimare/foco?branch=master&svg=true)](https://ci.appveyor.com/project/akashnimare/foco/branch/master)
+
+## Code style
+PEP8 Compliant
+
+ 
+## Screenshots
+Include logo/demo screenshot etc.
+
+## Installation
+TODO
+
+## Tests
+No test at the moment... SHAME!
+
+## How to use?
+If people like your project they’ll want to learn how they can use it. To do so include step by step guide to use your project.
+
+## Contribute
+Contributions are more than welcomed!
+
+## Credits
+Inspired by the AWS profile switcher.
+[awsp]()
+
+## License
+BSD
---
+++ awscon/awscon.py
@@ -0,0 +1,63 @@
+from boto3 import client
+from botocore.exceptions import ClientError, ProfileNotFound
+
+from PyInquirer import style_from_dict, Token, prompt
+from examples import custom_style_2
+
+import sys
+import subprocess
+
+def get_instances():
+  ec2_instances = []
+
+  try:
+    ec2client = client('ec2')
+    response = ec2client.describe_instances()
+  except ProfileNotFound as e:
+    print e.message
+    print "Make sure you defined and env var AWS_PROFILE pointing to your credentials"
+    sys.exit(1)
+  except NoRegionError as e:
+    print e.message
+    print "You need to define a region in your profile"
+    sys.exit(1)
+  
+  for ins in response['Reservations']:
+    instance = ins['Instances'][0]
+    instanceId = instance['InstanceId']
+  
+    if 'Tags' in instance:
+      name = [k['Value'] for k in instance['Tags'] if k['Key'] == 'Name']
+      if name == []:
+        name = ''
+      else:
+        name = name[0]
+    else:
+      name = '' 
+  
+    if 'PublicIpAddress' in instance:
+      publicAddress = instance['PublicIpAddress']
+    else:
+      publicAddress = ''
+  
+    privateAddress = instance['PrivateIpAddress']
+    entry = "%s - %s - %s - %s " % (instanceId, '{:<15}'.format(publicAddress), '{:<15}'.format(privateAddress), name)
+    ec2_instances.append(entry)
+
+  return ec2_instances
+
+def main():
+  prompt_list = [
+    {
+      'type': 'list',
+      'name': 'instance',
+      'message': 'Select instance to connect',
+      'choices': get_instances()
+    }]
+
+  answers = prompt(prompt_list, style=custom_style_2)
+  instanceId = answers['instance'].split(' - ')[0]
+  subprocess.call(["aws","ssm","start-session","--target",instanceId])
+
+if __name__ == '__main__':
+  main()
