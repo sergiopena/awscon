4c5c9059e12c2a9d1737c4458eae04b92e16ff8d
Sergio Pena <isergiopena@gmail.com>
2019-06-06 17:18:50 +0200

    Add region support and name filter

--- awscon/awscon.py
+++ awscon/awscon.py
@@ -1,20 +1,19 @@
 from boto3 import client
-from botocore.exceptions import ClientError, ProfileNotFound
+from botocore.exceptions import ClientError, ProfileNotFound, NoRegionError, EndpointConnectionError
 
 from PyInquirer import style_from_dict, Token, prompt
 from examples import custom_style_2
 
 import sys
 import subprocess
+import argparse
+import re
 
-
-import code
-
-def get_instances():
+def get_instances(args):
     ec2_instances = []
 
     try:
-        ec2client = client('ec2')
+        ec2client = client('ec2',region_name=args.region)
         response = ec2client.describe_instances()
     except ProfileNotFound as e:
         print e.message
@@ -25,6 +24,13 @@ def get_instances():
         print e.message
         print "You need to define a region in your profile"
         sys.exit(1)
+    except EndpointConnectionError as e:
+        print e.message
+        print "Check your region name, seems it's not reachable"
+        sys.exit(1)
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+        raise
 
     for ins in response['Reservations']:
         instance = ins['Instances'][0]
@@ -44,10 +50,10 @@ def get_instances():
         else:
             publicAddress = ''
 
-	if 'PrivateIpAddress' in instance:
-	    privateAddress = instance['PrivateIpAddress']
-	else:
-	    privateAddress = ''
+    	if 'PrivateIpAddress' in instance:
+	        privateAddress = instance['PrivateIpAddress']
+    	else:
+	        privateAddress = ''
 
         entry = "%s - %s - %s - %s " % (instanceId,
                                         '{:<15}'.format(publicAddress),
@@ -55,16 +61,20 @@ def get_instances():
                                         name)
         ec2_instances.append(entry)
 
+    if args.name != None:
+        r = re.compile('.* - .* - .* - .*'+args.name+'.*')
+        ec2_instances = filter(r.match, ec2_instances)
+
     return ec2_instances
 
 
-def main():
+def main(args):
     prompt_list = [
       {
         'type': 'list',
         'name': 'instance',
         'message': 'Select instance to connect',
-        'choices': get_instances()
+        'choices': get_instances(args)
       }]
 
     answers = prompt(prompt_list, style=custom_style_2)
@@ -73,4 +83,21 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        description='AWS SSM console manager',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument('--region', '-r', default='eu-west-1',
+                        help='Region to retrieve instances from')
+
+# TODO
+#    parser.add_argument('--instance', '-i', required=False,
+#                        help='Filter by instance id by applying .*arg.*')
+
+    parser.add_argument('--name', '-n', required=False,
+                        help='Filter by instance name by applying .*arg.*')
+
+    args = parser.parse_args()
+
+    main(args)
